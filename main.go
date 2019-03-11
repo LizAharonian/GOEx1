76f6a2197f1f1f21d342fb13a97a39ce53f04b1e
Liz <lizaharonian@gmail.com>
2019-03-11 23:16:08 +0200

    liz has been here

--- main.go
+++ main.go
@@ -1,4 +1,7 @@
-//submitted by Rocket dev team: Liz Aharonian, Raz Shenkman, Ori Ben Zaken.
+//submitted by Rocket dev team:
+// Liz Aharonian - 316584960
+// Raz Shenkman - 311130777
+// Ori Ben Zaken - 311492110
 
 // Fetch prints the content found at each specified URL.
 package main
@@ -6,7 +9,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -14,30 +16,17 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
+		//change url according to req
+		url = addPrefix(url)
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
-	}
-}
-
-func mission1() {
-	//io.Copy uses fixed 32KB buffer to copy from reader to writer until EOF.
-	//So no matter how big the source is, you’ll always just use 32KB to copy it to destination.
-	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
+		//print status code
+		fmt.Fprintf(os.Stdout, "resp.status %s\n", resp.Status)
+		//io.Copy uses fixed 32KB buffer to copy from reader to writer until EOF.
+		//So no matter how big the source is, you’ll always just use 32KB to copy it to destination.
 		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
@@ -47,47 +36,9 @@ func mission1() {
 	}
 }
 
-func mission2() {
-	for _, url := range os.Args[1:] {
-		//change url according to req
-		url = addPrefix(url)
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
-	}
-}
-
 func addPrefix(url string) string {
 	if !strings.HasPrefix(url, "http://") {
 		url = "http://" + url
 	}
 	return url
-}
-func mission3() {
-	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		//print status code
-		fmt.Fprintf(os.Stdout, "resp.status %s\n", resp.Status)
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
-	}
-
 }
